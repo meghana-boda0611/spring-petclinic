700c887a20301f2359e4882dbb65e3042901fb90
meghana-boda0611 <meghana.boda@gmail.com>
2025-02-16 12:42:27 -0500

    updated main.tf file to see if the issue is fixed.

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,10 +43,6 @@ resource "aws_eks_cluster" "eks" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster_role.arn
 
-  resource "aws_eks_cluster" "eks" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
-
   vpc_config {
     subnet_ids = module.vpc.private_subnets
   }
