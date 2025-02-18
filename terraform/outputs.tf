b80071625f83b5ee1fedc155bfe962f39744bfde
meghana-boda0611 <meghana.boda@gmail.com>
2025-02-18 11:01:48 -0500

    removed to create again

--- terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "cluster_id" {
-  value = aws_eks_cluster.eks.id
-}
-
-output "kubeconfig_command" {
-  value = "aws eks update-kubeconfig --region=${var.region} --name=${var.cluster_name}"
-}
-
