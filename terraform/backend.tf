1ed7ca3e84a8af3f572ab5761ac6eed4ed6160c2
MeghanaBoda <meghana.boda@gmail.com>
2025-06-11 14:18:43 -0400

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -15,6 +15,7 @@ resource "aws_s3_bucket_versioning" "versioning" {
   bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration {
     status = "Enabled"
+    mfa_delete = "Enabled"
   }
 }
 
